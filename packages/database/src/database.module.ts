3f04098267a70f958f74bf5c0a8aa379c977ae4c
pedroanastacio <pedro.anastacio013@gmail.com>
2026-03-20 17:09:03 -0300

    chore: init setup postgresql database

---
+++ packages/database/src/database.module.ts
@@ -0,0 +1,18 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DB_HOST || "localhost",
+      port: parseInt(process.env.DB_PORT || "5432"),
+      username: process.env.DB_USER || "postgres",
+      password: process.env.DB_PASSWORD || "postgres",
+      database: process.env.DB_NAME || "live-pool-test",
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+  ],
+})
+export class DatabaseModule {}
