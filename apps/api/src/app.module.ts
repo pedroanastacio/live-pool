9190e882da42bfd504c16b5963320454d7365b7a
pedroanastacio <pedro.anastacio013@gmail.com>
2026-03-22 13:50:41 -0300

    chore: rename packages prefix

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from '@repo/database';
+import { PrismaModule } from '@live-pool/database';
 
 @Module({
   imports: [
