8fc7f15f8a00f086e66da10c8186ebddb5073118
pedroanastacio <pedro.anastacio013@gmail.com>
2026-03-26 09:47:24 -0300

    feat: add api docs with swagger and scalar

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,14 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ValidationPipe } from '@nestjs/common/pipes';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('LivePool API')
+    .setDescription('API to manage polls and votes')
+    .setVersion('1.0')
+    .build();
+
+  const document = () => SwaggerModule.createDocument(app, config);
+
+  app.use(
+    '/api/docs',
+    apiReference({
+      content: document,
     }),
   );
 
