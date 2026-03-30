cb4548d39982612afd1e156b453a7780978befca
pedroanastacio <pedro.anastacio013@gmail.com>
2026-03-30 19:18:40 -0300

    fix: remove unnecessary assignment

--- apps/api/src/events/handlers/vote-cast.handler.ts
+++ apps/api/src/events/handlers/vote-cast.handler.ts
@@ -3,21 +3,13 @@ import { IEventHandler, IEvent } from '../types';
 import { VoteCastEvent } from '../classes';
 import { Producer, QUEUES } from '@live-pool/messaging';
 import { PRODUCER_TOKEN } from '../../messaging/messaging.module';
-import { CreateVoteDto } from 'src/modules/votes/dto';
 
 @Injectable()
 export class VoteCastEventHandler implements IEventHandler<IEvent> {
   constructor(@Inject(PRODUCER_TOKEN) private producer: Producer) {}
 
   handle(event: VoteCastEvent): void {
-    const voteData = event.data as CreateVoteDto;
-
-    const message = {
-      pollId: voteData.pollId,
-      pollOptionId: voteData.pollOptionId,
-    };
-
-    this.producer.publish(QUEUES.VOTE_CAST.routingKey, message);
-    console.log('VoteCastEventHandler: Message published to queue', message);
+    this.producer.publish(QUEUES.VOTE_CAST.routingKey, event.data);
+    console.log('VoteCastEventHandler: Message published to queue', event.data);
   }
 }
